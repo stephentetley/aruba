a118fb8c7817d523a7074911617b85a6cf08f8bf
stephentetley <stephen.tetley@gmail.com>
2018-11-27 21:26:55 +0000

    dummy_traversals - implemented choose with catch so it better handles failure (e.g. type failure).

--- src/dummy_traversals.pl
+++ src/dummy_traversals.pl
@@ -28,57 +28,60 @@
 
 % choose(r1,r1)
 
-identity_r(A,A).
+identity_rewrite(Ans, Ans).
 
-fail_r(_) :- false.
+fail_rewrite(_) :- false.
 
-seq_r(R1, R2, Input, Ans) :-
+seq_rewrite(R1, R2, Input, Ans) :-
     call(R1, Input, A1),
     call(R2, A1, Ans).
 
 add1(X,Y) :- Y is X + 1.
 
-demo01(Ans) :- seq_r(add1, add1, 10,Ans).
+demo01(Ans) :- seq_rewrite(add1, add1, 10,Ans).
 
-choose_r(R1, R2, Input, Ans) :-
-    ( call(R1,Input,X) -> Ans is X
-    ; call(R2,Input,Ans)
-    ).
+choose_rewrite(R1, R2, Input, Ans) :-
+    catch(call(R1,Input,Ans), 
+          _, 
+          call(R2,Input,Ans)).
 
 if_even_add10(X,Y) :- 
     A1 is rem(X,2),
     (A1 == 0 -> Y is X + 10).
 
 
-demo02(Ans) :- choose_r(if_even_add10, add1, 1, Ans).
+demo02(Ans) :- choose_rewrite(if_even_add10, add1, 1, Ans).
 
 employee(person("stephen", "Yorkshire"), 10000000.0).
 
 demo03(Ans) :- employee(person("stephen", "Yorkshire"), 10000000.0) =.. Ans.
 
-all_r_aux(_,[],[]).
-all_r_aux(R1, [X|Xs], Ans) :-
+all_rewrite_aux(_,[],[]).
+all_rewrite_aux(R1, [X|Xs], Ans) :-
     call(R1, X, A1),
-    all_r_aux(R1, Xs, A2),
+    all_rewrite_aux(R1, Xs, A2),
     Ans = [A1|A2].
 
 
-all_r(R1,Input,Ans) :-
+all_rewrite(R1,Input,Ans) :-
     Input =.. [Head|Kids],
-    all_r_aux(R1, Kids, Kids1), 
+    all_rewrite_aux(R1, Kids, Kids1), 
     Ans =.. [Head|Kids1], !.
 
 
-demo04(Ans) :- all_r(identity_r, employee(person("stephen", "Yorkshire"), 10000000.0), Ans).
+demo04(Ans) :- all_rewrite(identity_rewrite, employee(person("stephen", "Yorkshire"), 10000000.0), Ans).
 
-
+% Prolog lists are heterogenous (of course!)...
 demo05(Ans) :- 
     Ans = [1,"two"].
 
 
+try_rewrite(R1,Input,Ans) :-
+    choose_rewrite(R1, identity_rewrite, Input, Ans).
+
 
     
-    
-
+demo06(Input, Ans) :- 
+    try_rewrite(add1, Input, Ans).
     
 
